d9f92603ff47ead641276f6d8840d81602257ebc
badogan <basri.dogan.71@gmail.com>
2019-11-20 16:35:49 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,25 @@ require 'pry'
 
 class String
 
+  attr_accessor :sentences_counted
+
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    delimiters = ['!', '.','?']
+    self.sentences_counted = self.split(Regexp.union(delimiters))
+    self.sentences_counted = self.sentences_counted.reject { |each_entry_in_array| each_entry_in_array.empty? }
+    self.sentences_counted.count
   end
 end
+
