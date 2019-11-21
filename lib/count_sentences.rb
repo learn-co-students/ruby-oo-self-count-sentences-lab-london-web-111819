9e15e3c96385dcde8bc235fab2c99048ceea289a
Matthew <matthew@Matthews-MacBook-Pro.local>
2019-11-21 09:13:40 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,34 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?('.')
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?('?')
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?('!')
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    words = self.split(/\?|\.|!/)
+    words.reject! do |word|
+      word.empty?
+    end
+    words.length
   end
 end
